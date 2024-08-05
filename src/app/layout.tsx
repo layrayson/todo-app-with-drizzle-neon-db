c892507b551811d8056f4ddeaf36090b7bde9509
Abdulazeez Olalere <olalereazeez11@gmail.com>
2024-08-05 17:01:50 +0100

    FEAT: setup MUI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import RTQueryClient from "@/lib/components/wrappers/RTQueryClient";
+import { MUIThemeProvider } from "@/lib/components/wrappers/MuiThemeProvide";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <RTQueryClient>{children}</RTQueryClient>
+        <RTQueryClient>
+          <MUIThemeProvider>{children}</MUIThemeProvider>
+        </RTQueryClient>
       </body>
     </html>
   );
---
+++ src/lib/components/wrappers/MuiThemeProvide.tsx
@@ -0,0 +1,10 @@
+import { createTheme, ThemeProvider } from "@mui/material";
+import { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+const theme = createTheme();
+export const MUIThemeProvider = ({ children }: Props) => {
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+};
