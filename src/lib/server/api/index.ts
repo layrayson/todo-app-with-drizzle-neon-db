76db2221436a745d41170aa45a06959615715ae2
Abdulazeez Olalere <olalereazeez11@gmail.com>
2024-08-05 16:59:52 +0100

    FEAT: create API service

--- src/lib/server/api/index.ts
+++ src/lib/server/api/index.ts
@@ -1,48 +1,56 @@
 import { eq } from "drizzle-orm";
 import { db } from "../db";
 import { todos } from "../schema";
+import {
+  CreateTodoInput,
+  DeleteTodoInput,
+  FetchTodosResponse,
+  UpdateTodoInput,
+} from "@/lib/types";
 
 const handleError = (error: any) => {
   throw new Error("An error occurred while processing your request.");
 };
 
-export const fetchTodos = async () => {
-  try {
-    return await db.select().from(todos).execute();
-  } catch (error) {
-    handleError(error);
-  }
-};
+export class TodoService {
+  fetchTodos = async (): Promise<FetchTodosResponse | undefined> => {
+    try {
+      return await db.select().from(todos).execute();
+    } catch (error) {
+      handleError(error);
+    }
+  };
 
-export const addTodo = async (task: string) => {
-  try {
-    return await db.insert(todos).values({ task }).returning().execute();
-  } catch (error) {
-    handleError(error);
-  }
-};
+  addTodo = async ({ task }: CreateTodoInput) => {
+    try {
+      return await db.insert(todos).values({ task }).returning().execute();
+    } catch (error) {
+      handleError(error);
+    }
+  };
 
-export const updateTodo = async (
-  id: number,
-  task: string,
-  completed: boolean
-) => {
-  try {
-    return await db
-      .update(todos)
-      .set({ task, completed })
-      .where(eq(todos.id, id))
-      .returning()
-      .execute();
-  } catch (error) {
-    handleError(error);
-  }
-};
+  updateTodo = async ({ id, task, completed }: UpdateTodoInput) => {
+    try {
+      return await db
+        .update(todos)
+        .set({ task, completed })
+        .where(eq(todos.id, id))
+        .returning()
+        .execute();
+    } catch (error) {
+      handleError(error);
+    }
+  };
 
-export const deleteTodo = async (id: number) => {
-  try {
-    return await db.delete(todos).where(eq(todos.id, id)).returning().execute();
-  } catch (error) {
-    handleError(error);
-  }
-};
+  deleteTodo = async ({ id }: DeleteTodoInput) => {
+    try {
+      return await db
+        .delete(todos)
+        .where(eq(todos.id, id))
+        .returning()
+        .execute();
+    } catch (error) {
+      handleError(error);
+    }
+  };
+}
