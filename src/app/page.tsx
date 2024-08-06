984a480630ef361bfc91f1ec83829d5af3e33224
Abdulazeez Olalere <olalereazeez11@gmail.com>
2024-08-06 12:38:54 +0100

    FEAT: update favicon and title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import TodoList from "@/lib/components/TodoList";
 import { SectionPadding } from "@/lib/components/wrappers/SectionPadding";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: `Todo App`,
+  description: `Write your Todos`,
+};
 
 export default function Home() {
   return (
